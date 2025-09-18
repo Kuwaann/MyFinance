72fa8bc9d873089bef50b791009ea59fc998aa34
WeHusky <manaadacuy18@gmail.com>
2025-09-18 23:47:23 +0700

    keren

--- lib/kelompok.dart
+++ lib/kelompok.dart
@@ -6,82 +6,91 @@ class TeamPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 10, 10, 17),
-      body: SafeArea(
-        child: Column(
-          children: [
-            const SizedBox(height: 30),
-            const Text(
-              "OUR TEAMS",
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 22,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 20),
-
-            // Daftar anggota kelompok
-            Expanded(
-              child: ListView(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                children: const [
-                  TeamMember(
-                    name: "Acchmad Nur Choiron",
-                    nim: "124230126",
-                    imagePath: "assets/images/roblox.jpeg",
-                  ),
-                  SizedBox(height: 16),
-                  TeamMember(
-                    name: "Muhammad Sulthan Al Azzam",
-                    nim: "124230127",
-                    imagePath: "assets/images/roblox.jpeg",
-                  ),
-                  SizedBox(height: 16),
-                  TeamMember(
-                    name: "Muhammad Emir Rivaldy",
-                    nim: "124230135",
-                    imagePath: "assets/images/roblox.jpeg",
-                  ),
-                  SizedBox(height: 16),
-                  TeamMember(
-                    name: "Ridho Nur Maulana",
-                    nim: "124230142",
-                    imagePath: "assets/images/roblox.jpeg",
-                  ),
-                ],
-              ),
-            ),
-
-            // Tombol kembali ke menu
-            Padding(
-              padding: const EdgeInsets.only(bottom: 20),
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.pushReplacementNamed(context, '/homepage');
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 40,
-                    vertical: 15,
-                  ),
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [AppColors.backgroundLogin, Color.fromARGB(255, 30, 30, 59)],
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SafeArea(
+          child: Column(
+            children: [
+              const SizedBox(height: 30),
+              const Text(
+                "OUR TEAMS",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
                 ),
-                child: const Text(
-                  "Kembali ke Menu",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w900,
-                  ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+
+              // Daftar anggota kelompok
+              Expanded(
+                child: ListView(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  children: const [
+                    TeamMember(
+                      name: "Acchmad Nur Choiron",
+                      nim: "124230126",
+                      imagePath: "assets/images/roblox.jpeg",
+                    ),
+                    SizedBox(height: 16),
+                    TeamMember(
+                      name: "Muhammad Sulthan Al Azzam",
+                      nim: "124230127",
+                      imagePath: "assets/images/roblox.jpeg",
+                    ),
+                    SizedBox(height: 16),
+                    TeamMember(
+                      name: "Muhammad Emir Rivaldy",
+                      nim: "124230135",
+                      imagePath: "assets/images/roblox.jpeg",
+                    ),
+                    SizedBox(height: 16),
+                    TeamMember(
+                      name: "Ridho Nur Maulana",
+                      nim: "124230142",
+                      imagePath: "assets/images/roblox.jpeg",
+                    ),
+                  ],
                 ),
               ),
-            ),
-          ],
+
+              // Tombol kembali ke menu
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushReplacementNamed(context, '/homepage');
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 40,
+                      vertical: 15,
+                    ),
+                  ),
+                  child: const Text(
+                    "Kembali ke Menu",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
